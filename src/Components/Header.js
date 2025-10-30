4173e3a4e11ec50bf8e3da3ea46bf4f0f4a13d21
saravanan642 <saravanansenthil605@gmail.com>
2025-10-30 22:37:21 +0530

    header Tcomplite

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,36 +7,36 @@ import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 const Header = () => {
   return (
-    <div>
+    <div >
       <div className=' flex gap-[15px] border border-none '>
-        <div className="flex  gap-[20px] text-xl m-[15px] ml-[5%]">
+        <div className="flex  gap-[20px] text-l m-[15px] ml-[5%]">
           <MdOutlineMarkEmailUnread className=" mt-[5px] text-sky-600" />
           <p className=' font-light'>Villa@company.com</p>
         </div>
 
 
-        <div className=" flex  gap-[20px] text-xl m-[15px] ml-[5%]">
+        <div className=" flex  gap-[20px] text-l m-[15px] ml-[5%]">
           <TiWavesOutline className=" mt-[5px] text-sky-600" />
           <p className=' font-light'> Sunny Isles Beach, FL 33160</p>
         </div>
-      </div>
-      
-      <div>
 
-        <div>
-          <FaFacebook />
+        <div className='mt-[20px] flex gap-[60px]  text-2xl   ml-[35%] justify-evenly' >
 
-        </div>
+          <div>
+            <FaFacebook />
 
-        <div>
-          <FaLinkedin />
-        </div>
+          </div>
 
-        <div>
-          <FaTwitter />
-        </div>
-        <div>
-          <FaInstagram />
+          <div>
+            <FaLinkedin />
+          </div>
+
+          <div>
+            <FaTwitter />
+          </div>
+          <div>
+            <FaInstagram />
+          </div>
         </div>
       </div>
       <hr></hr>
