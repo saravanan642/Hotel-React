1e2cab4e888c1108d62358fcc06e7952469bb6ee
saravanan642 <saravanansenthil605@gmail.com>
2025-10-30 22:13:19 +0530

    Heapder email upadte

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { TiWavesOutline } from "react-icons/ti";
 
 const Header = () => {
   return (
     <div>
-      <div className="flex  gap-[20px] text-xl m-[15px] ">
-        <MdOutlineMarkEmailUnread className=" mt-[5px] text-sky-600" />
-        <p className=' font-light'>Villa@company.com</p>
-      </div> 
+      <div className=' flex gap-[15px] border border-none '>
+        <div className="flex  gap-[20px] text-xl m-[15px] ml-[5%]">
+          <MdOutlineMarkEmailUnread className=" mt-[5px] text-sky-600" />
+          <p className=' font-light'>Villa@company.com</p>
+        </div>
+
+
+        <div className=" flex  gap-[20px] text-xl m-[15px] ml-[5%]">
+          <TiWavesOutline className=" mt-[5px] text-sky-600" />
+          <p className=' font-light'> Sunny Isles Beach, FL 33160</p>
+        </div>
+      </div>
       <hr></hr>
 
     </div>
