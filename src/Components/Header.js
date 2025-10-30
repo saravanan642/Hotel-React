2e2d6548b7f988ddd01a8d8f3e13068165157acd
saravanan642 <saravanansenthil605@gmail.com>
2025-10-30 22:19:07 +0530

    icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { TiWavesOutline } from "react-icons/ti";
-
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Header = () => {
   return (
     <div>
@@ -15,6 +18,25 @@ const Header = () => {
         <div className=" flex  gap-[20px] text-xl m-[15px] ml-[5%]">
           <TiWavesOutline className=" mt-[5px] text-sky-600" />
           <p className=' font-light'> Sunny Isles Beach, FL 33160</p>
+        </div>
+      </div>
+      
+      <div>
+
+        <div>
+          <FaFacebook />
+
+        </div>
+
+        <div>
+          <FaLinkedin />
+        </div>
+
+        <div>
+          <FaTwitter />
+        </div>
+        <div>
+          <FaInstagram />
         </div>
       </div>
       <hr></hr>
