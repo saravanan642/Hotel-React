3824740bd89846091988fa622fe85e0cbe16d5d8
saravanan642 <saravanansenthil605@gmail.com>
2025-10-30 22:02:10 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,11 @@ import { MdOutlineMarkEmailUnread } from "react-icons/md";
 const Header = () => {
   return (
     <div>
-      <div className="flex  gap-2 ">
-        <MdOutlineMarkEmailUnread className="" />
-        <p>Villa@company.com</p>
-      </div>
+      <div className="flex  gap-[20px] text-xl m-[15px] ">
+        <MdOutlineMarkEmailUnread className=" mt-[5px] text-sky-600" />
+        <p className=' font-light'>Villa@company.com</p>
+      </div> 
+      <hr></hr>
 
     </div>
   )
