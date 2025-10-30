d0e9d45c48d927d28c663a21c5759aa1c09af285
saravanan642 <saravanansenthil605@gmail.com>
2025-10-30 21:09:53 +0530

    FIrst commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <MdOutlineMarkEmailUnread />
+        <p> info@company.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
