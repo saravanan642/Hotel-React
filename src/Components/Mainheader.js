bfa242a7243ec07e3e90c98239b52bf8e6ece3dc
saravanan642 <saravanansenthil605@gmail.com>
2025-11-03 21:05:59 +0530

    UPadate MainHeader.js

--- src/Components/Mainheader.js
+++ src/Components/Mainheader.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import { MdContactEmergency } from "react-icons/md";
 
 const Mainheader = () => {
     return (
-        <div className='  border border-none m-[1px] flex justify-between'>
+        <div className='  border border-none flex justify-between'>
             <div>
-                <p className=' font-bold border border-none  mt-[30px] text-3xl ml-[120px] '>Villa</p>
+                <p className=' font-semibold border border-none  mt-[30px] text-3xl ml-[120px] '>Villa</p>
                 </div>
 
-                <div className='flex gap-[70px] font-bold border border-none mt-[30px] text-2xl mr-[60px]'>
+                <div className='flex gap-[70px] font-semibold border border-none mt-[30px] text-xl ml-[20%]'>
                     <a href='/'>Home</a>
-                    <a href='/'>Home</a>
-                    <a href='/'>Home</a>
-                    <a href='/'>Home</a>
+                    <a href='/'>Properties</a>
+                    <a href='/'>Property Deatils</a>
+                    <a href='/'>About</a>
+                    
+                </div>
+
+                <div className=" border border-none flex ">
+                    
+                    <button className='font-semibold border border-solid mt-[30px] text-xl mr-[100px] pr-[30px] py-[5px] flex  items-center gap-[20px]' >
+                       < MdContactEmergency className=' m-[4px]'/> 
+                        Visit Us
+                        </button>
                 </div>
 
             
