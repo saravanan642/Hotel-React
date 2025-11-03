54f760862b36e3f6f78392ddc05ae39a3a009634
saravanan642 <saravanansenthil605@gmail.com>
2025-11-03 11:48:48 +0530

    updated Header.js

--- src/Components/Mainheader.js
+++ src/Components/Mainheader.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 const Mainheader = () => {
-  return (
-    <div>
-      <div>
-    <p className=' font-bold border border-none px-[90px] py-[50px] text-3xl '>Villa</p>
+    return (
+        <div>
+            <div>
+                <p className=' font-bold border border-none px-[90px] py-[50px] text-3xl '>Villa</p>
 
-      </div>
-    </div>
-  )
+
+                <a href='/'>Home</a>
+                <a href='/'>Home</a>
+                <a href='/'>Home</a>
+                <a href='/'>Home</a>
+            </div>
+        </div>
+    )
 }
 
 export default Mainheader
