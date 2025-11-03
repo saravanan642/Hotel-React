a57fbc532d68d6de0bbd96679e7a9c6d1ec2324e
saravanan642 <saravanansenthil605@gmail.com>
2025-11-03 11:54:59 +0530

    updated Header.js

--- src/Components/Mainheader.js
+++ src/Components/Mainheader.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Mainheader = () => {
     return (
-        <div>
+        <div className=' flex'>
             <div>
                 <p className=' font-bold border border-none px-[90px] py-[50px] text-3xl '>Villa</p>
 
+                <div className='flex gap-[30px] font-bold border border-none px-[90px] py-[50px] text-2xl'>
+                    <a href='/'>Home</a>
+                    <a href='/'>Home</a>
+                    <a href='/'>Home</a>
+                    <a href='/'>Home</a>
+                </div>
 
-                <a href='/'>Home</a>
-                <a href='/'>Home</a>
-                <a href='/'>Home</a>
-                <a href='/'>Home</a>
             </div>
         </div>
     )
