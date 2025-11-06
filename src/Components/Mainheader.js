5fe47183be15ca6cc3fe841819fdad58cf332c86
saravanan642 <saravanansenthil605@gmail.com>
2025-11-06 21:21:22 +0530

    Mainheader.js

--- src/Components/Mainheader.js
+++ src/Components/Mainheader.js
@@ -6,25 +6,24 @@ const Mainheader = () => {
         <div className='  border border-none flex justify-between'>
             <div>
                 <p className=' font-semibold border border-none  mt-[30px] text-3xl ml-[120px] '>Villa</p>
-                </div>
+            </div>
+            <div className='flex gap-[70px] font-semibold border-none mt-[30px] text-xl ml-[20%]'>
+                <a href='/' className='hover:text-red-600 transition-colors duration-300'>Home</a>
+                <a href='/' className='hover:text-red-600 transition-colors duration-300'>Properties</a>
+                <a href='/' className='hover:text-red-600 transition-colors duration-300'>Property Details</a>
+                <a href='/' className='hover:text-red-600 transition-colors duration-300'>About</a>
+            </div>
 
-                <div className='flex gap-[70px] font-semibold border border-none mt-[30px] text-xl ml-[20%]'>
-                    <a href='/'>Home</a>
-                    <a href='/'>Properties</a>
-                    <a href='/'>Property Deatils</a>
-                    <a href='/'>About</a>
-                    
-                </div>
 
-                <div className=" border border-none flex ml-[70px] ">
-                    
-                    <button className='font-semibold border border-solid mt-[25px] text-xl mr-[100px] pr-[30px]  py-[5px] flex  items-center gap-[20px] bg-green-600 hover:text-rose-700 transition'  >
-                       < MdContactEmergency className=' m-[4px] text-blue-900  text-2xl'/> 
-                        Visit Us
-                     </button>
-                </div>
+            <div className=" border border-none flex ml-[70px] ">
 
-            
+                <button className='font-semibold border border-solid mt-[25px] text-xl mr-[100px] pr-[30px]  py-[5px] flex  items-center gap-[20px] bg-green-600 hover:text-rose-700 transition'  >
+                    < MdContactEmergency className=' m-[4px] text-blue-900  text-2xl' />
+                    Visit Us
+                </button>
+            </div>
+
+
         </div>
     )
 }
