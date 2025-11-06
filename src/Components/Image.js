fdeca4a403e9031e094059ad123e05aa04c7cc5e
saravanan642 <saravanansenthil605@gmail.com>
2025-11-06 22:12:33 +0530

    Sunset.js

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,12 +1,33 @@
+import Slider from "react-slick"; 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
-import imge1 from "../Assests/image.jpj.png"
+// images import
+import imge1 from "../Assests/image.jpj.png";
+import image2 from "../Assests/Mainimage.jpg";
+import image3 from "../Assests/sunset.jpg";
 
 const Image = () => {
-  return (
-    <div>
-      <img src={imge1} alt='imd1' className='w-[1550px] h-[640px] pt-[20px]'></img>
-    </div>
-  )
-}
+  // Slider settings
+  const settings = {
+    dots: true,           // கீழே dots வரும்
+    infinite: true,       // loop ஆகும்
+    speed: 500,           // transition speed
+    slidesToShow: 1,      // ஒரே படம் மட்டும்
+    slidesToScroll: 1,    // ஒரு படம் மாறும்
+    autoplay: true,       // auto slide
+    autoplaySpeed: 3000,  // 2 second-க்கு ஒருமுறை மாறும்
+  };
 
-export default Image
+  return (
+    <div className="w-full max-w-[1550px] mx-auto mt-[20px]">
+      <Slider {...settings}>
+        <img src={imge1} alt="img1" className="w-full h-[640px] object-cover" />
+        <img src={image2} alt="img2" className="w-full h-[640px] object-cover" />
+        <img src={image3} alt="img3" className="w-full h-[640px] object-cover" />
+      </Slider>
+    </div>
+  );
+};
+
+export default Image;
