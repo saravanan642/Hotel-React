269389000d874a7a9f943210f92c108f90fe0091
saravanan642 <saravanansenthil605@gmail.com>
2025-11-14 20:16:03 +0530

    updated  Property.js

--- src/Components/Property.js
+++ src/Components/Property.js
@@ -1,14 +1,20 @@
-import img4 from "../Assests/ban-rak-thai-mae-hong-son-thailand.jpg"
+import img4 from "../Assests/ban-rak-thai-mae-hong-son-thailand.jpg";
 
 const Property = () => {
   return (
-    <div>
-      <div>
-        <img src={ img4 } alt="Property"  className=" w-[100%] h-[550px]"></img>
-      </div>
+    <div className="relative py-[3%]">
+      
+      {/* Background Image */}
+      <div
+        className="h-[500px] bg-cover bg-center brightness-50 "
+        style={{ backgroundImage: `url(${img4})` }}
+      ></div>
+
+      {/* Black Overlay */}
+      <div className="absolute top-[8%] left-0 w-full h-[500px] bg-black/35"></div>
 
     </div>
-  )
-}
+  );
+};
 
-export default Property
+export default Property;
