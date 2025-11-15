e913f09837509cc1dc75bbd5ae53c7347e631c96
saravanan642 <saravanansenthil605@gmail.com>
2025-11-15 06:19:22 +0530

    updated  Property.js

--- src/Components/Property.js
+++ src/Components/Property.js
@@ -12,9 +12,9 @@ const Property = () => {
 
       {/* Black Overlay */}
       <div className="absolute top-[8%] left-0 w-full h-[500px] bg-black/35"></div>
-      <div  className="  absolute  top-[40%] left-[38%] font-bold  text-7xl text-white  ">
-        <h1>Property</h1>
-        <button className=" text-3xl absoute ml-[20%] mt-[30px]"> Explore Now</button>
+      <div  className="  absolute  top-[39%] left-[38%] font-bold  text-7xl text-gray-200  ">
+        <h1 className=" text-gray-200">Property</h1>
+        <button className=" text-3xl absoute ml-[20%] mt-[30px] border border-border-solid p-[8px]  hover:bg-slate-950 transition-colors duration-7500"> Explore Now</button>
       </div>
 
     </div>
