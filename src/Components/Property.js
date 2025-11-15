eecc9a460b38262aa5ab8f8c721d7519c8a23372
saravanan642 <saravanansenthil605@gmail.com>
2025-11-15 06:38:03 +0530

    updated  Property.js

--- src/Components/Property.js
+++ src/Components/Property.js
@@ -2,16 +2,16 @@ import img4 from "../Assests/ban-rak-thai-mae-hong-son-thailand.jpg";
 
 const Property = () => {
   return (
-    <div className="relative py-[3%]">
+  <div className="relative py-[3%] px-[10px] border border-none  rounded-[20px] overflow-hidden">
       
       {/* Background Image */}
       <div
-        className="h-[500px] bg-cover bg-center brightness-50 "
+        className="h-[500px] bg-cover bg-center brightness-50  rounded-[20px]"
         style={{ backgroundImage: `url(${img4})` }}
       ></div>
 
       {/* Black Overlay */}
-      <div className="absolute top-[8%] left-0 w-full h-[500px] bg-black/35"></div>
+      <div className="absolute top-[8%] left-0 mr-[130px] ml-[10px] w-full h-[500px] bg-black/35"></div>
       <div  className="  absolute  top-[39%] left-[38%] font-bold  text-7xl text-gray-200  ">
         <h1 className=" text-gray-200">Property</h1>
         <button className=" text-3xl absoute ml-[20%] mt-[30px] border border-border-solid p-[8px]  hover:bg-slate-950 transition-colors duration-7500"> Explore Now</button>
